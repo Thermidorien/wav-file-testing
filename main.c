71e11c81610acdb7239f0d5d77be0e87fa6868df
Thermidorien <gotter.dammerung@outlook.com>
2024-01-02 00:35:21 -0500

    populated wav header

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "stdio.h";
 #include "stdint.h";
+#include "string.h";
+#include "math.h";
+
+#define M_PI (3.14159265358979323846)
 
 struct wav_header {
 
@@ -20,3 +24,34 @@ struct wav_header {
     int32_t data_length;
 };
 
+int main() {
+    const int sample_rate = 8000;
+
+
+
+    struct wav_header wavh;
+
+    // Look up https://docs.fileformat.com/audio/wav/ for more info on this
+    strncpy(wavh.riff,"RIFF",4);
+    strncpy(wavh.wave,"WAVE",4);
+    strncpy(wavh.format,"fmt ",4);
+    strncpy(wavh.data,"data",4);
+
+    wavh.chunk_size = 16;
+    wavh.format_tag = 1;
+    wavh.num_channels = 1;
+    wavh.sample_rate = sample_rate;
+    wavh.bits_per_sample = 16;
+    wavh.bytes_per_sample = (wavh.bits_per_sample / 8) * wavh.num_channels;
+    wavh.bytes_per_second = wavh.sample_rate * wavh.bytes_per_sample;
+
+    const int duration_seconds = 10;
+    const int buffer_size = sample_rate * duration_seconds;
+    const int header_length = 44;
+
+    wavh.data_length = buffer_size * wavh.bytes_per_sample;
+    wavh.file_length = wavh.data_length + header_length;
+ 
+   
+    return 0;
+}
